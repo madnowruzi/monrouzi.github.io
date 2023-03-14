c3a3c38fb25cdf361a451ed1fa83da1f349f811b
Mohamad Norouzi <monrouzi@proton.me>
2023-03-14 05:03:24 +0330

    'wip'

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
+import vercel from '@astrojs/vercel/static';
 
 export default defineConfig({
-  integrations: [tailwind(), mdx()]
+  integrations: [tailwind(), mdx()],
+  output: "server",
+  adapter: vercel({
+    analytics: true
+  })
 });
